8bf0407a77e1747b08933e77fea053299d21288a
Дмитрий Сердюков <dimaserd84@gmail.com>
2023-07-14 22:12:48 +0300

    Fixes

--- projects/croco-generic-app-logic/src/public-api.ts
+++ projects/croco-generic-app-logic/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of croco-generic-app-logic
  */
 
-export * from './lib/croco-generic-app-logic.service';
-export * from './lib/croco-generic-app-logic.component';
 export * from './lib/croco-generic-app-logic.module';
