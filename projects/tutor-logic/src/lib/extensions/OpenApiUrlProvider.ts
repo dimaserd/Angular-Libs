7670a33b7bff6a5254ae2b1befefe0ab5863ce83
Дмитрий Сердюков <dimaserd84@gmail.com>
2023-09-16 14:08:02 +0300

    Fixes

--- projects/tutor-logic/src/lib/extensions/OpenApiUrlProvider.ts
+++ projects/tutor-logic/src/lib/extensions/OpenApiUrlProvider.ts
@@ -8,19 +8,28 @@ import { OpenApiServerOptions } from "../models";
 export class OpenApiUrlProvider {
 
   _urlValue: string = "";
+  _data: OpenApiServerOptions = {
+    useOpenApiServer: false,
+    openApiUrl: ""
+  }
 
   constructor(private _httpClient: HttpClient,
     @Inject('BASE_URL') baseUrl: string) {
     this._urlValue = baseUrl;
-    this.getSettings();
+    this.getSettings(baseUrl);
   }
 
-  getSettings() {
-    this._httpClient.get<OpenApiServerOptions>(this._urlValue + "api/settings/open-api/get").subscribe(data => {
+  getSettings(baseUrl: string) {
+    this._httpClient.get<OpenApiServerOptions>(baseUrl + "api/settings/open-api/get").subscribe(data => {
       if(data.useOpenApiServer && data.openApiUrl){
+        this._data = data;
         this._urlValue = data.openApiUrl;
       }
     });
+  }
+
+  getData() {
+    return this._data;
   }
 
   getUrl(): string {
