334da2172bb803a42606f214f5a94d9bbe03c4af
Дмитрий Сердюков <dimaserd84@gmail.com>
2023-11-02 18:55:17 +0300

    Fixes

--- projects/tutor-logic/src/lib/models/group-models.ts
+++ projects/tutor-logic/src/lib/models/group-models.ts
@@ -1,56 +1,56 @@
 import { StudentSimpleModel } from "./student-models";
 
 export interface SearchStudentGroups {
-    q: string; 
-    count: number | null; 
-    offSet: number; 
+    q: string;
+    count: number | null;
+    offSet: number;
 }
 
 export interface StudentGroupSimpleModel {
-    id: string; 
-    name: string; 
-    studentCount: number; 
-    isDeleting: boolean; 
-    useHtmlForName: boolean; 
-    nameHtml: string; 
+    id: string;
+    name: string;
+    studentCount: number;
+    isDeleting: boolean;
+    useHtmlForName: boolean;
+    nameHtml: string;
 }
 
 export interface StudentGroupDetailedModel {
-    id: string; 
-    name: string; 
-    enterCode: string; 
-    useCourse: boolean; 
-    studentCount: number; 
-    solutionsCount: number; 
-    isDeleting: boolean; 
-    useHtmlForName: boolean; 
-    nameHtml: string; 
-    useSchedule: boolean; 
-    scheduleHtml: string; 
-    useMaterials: boolean; 
-    materialsHtml: string; 
+    id: string;
+    name: string;
+    enterCode: string;
+    useCourse: boolean;
+    studentCount: number;
+    solutionsCount: number;
+    isDeleting: boolean;
+    useHtmlForName: boolean;
+    nameHtml: string;
+    useSchedule: boolean;
+    scheduleHtml: string;
+    useMaterials: boolean;
+    materialsHtml: string;
 }
 
 export interface StudentInGroupSimpleModel {
-    student: StudentSimpleModel; 
-    isBlocked: boolean; 
-    shouldBlockOnUtc: string; 
-    enteredOnUtc: string; 
+    student: StudentSimpleModel;
+    isBlocked: boolean;
+    shouldBlockOnUtc: string;
+    enteredOnUtc: string;
 }
 
 export interface SearchStudentsInGroup {
-    groupId: string; 
-    q: string; 
-    hasIndividualPaymentPlans: boolean | null; 
-    isBlocked: boolean | null; 
-    count: number | null; 
-    offSet: number; 
+    groupId: string;
+    q: string;
+    hasIndividualPaymentPlans: boolean | null;
+    isBlocked: boolean | null;
+    count: number | null;
+    offSet: number;
 }
 
 export interface StudentTestGroupRelationDetailedModel {
-    studentGroupId: string; 
-    testId: string; 
-    canCheckSingleQuestion: boolean; 
+    studentGroupId: string;
+    testId: string;
+    canCheckSingleQuestion: boolean;
     html: string;
     showHtml: boolean;
     testName: string;
@@ -58,25 +58,25 @@ export interface StudentTestGroupRelationDetailedModel {
 }
 
 export interface SearchStudentGroupsByStudentRequest {
-    studentId: string; 
-    q: string; 
-    count: number | null; 
-    offSet: number; 
+    studentId: string;
+    q: string;
+    count: number | null;
+    offSet: number;
 }
 
 export interface StudentGroupWithCourseProgressModel {
-    groupId: string; 
-    groupName: string; 
-    groupUseHtmlForName: boolean; 
-    groupNameHtml: string; 
-    isBlocked: boolean; 
-    shouldBlockOnUtc: Date | null; 
-    courseProgress: StudentGroupCourseProgressSimpleModel; 
+    groupId: string;
+    groupName: string;
+    groupUseHtmlForName: boolean;
+    groupNameHtml: string;
+    useCourse: boolean;
+    isBlocked: boolean;
+    shouldBlockOnUtc: Date | null;
+    courseProgress: StudentGroupCourseProgressSimpleModel;
 }
 
-
 export interface StudentGroupCourseProgressSimpleModel {
-    totalWeight: number; 
-    currentProgress: number; 
-    progressPercents: number; 
+    totalWeight: number;
+    currentProgress: number;
+    progressPercents: number;
 }
