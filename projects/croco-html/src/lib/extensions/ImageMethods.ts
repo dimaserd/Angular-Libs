95e65e8d6c6ffca354d89abe3ee7106280ac6c5b
Дмитрий Сердюков <dimaserd84@gmail.com>
2024-07-29 15:12:56 +0300

    Fixes

--- projects/croco-html/src/lib/extensions/ImageMethods.ts
+++ projects/croco-html/src/lib/extensions/ImageMethods.ts
@@ -5,30 +5,34 @@ export class FileImageTagDataConsts {
     static FileIdAttrName = "file-id";
 }
 
-export interface FileImageTag{
+export interface FileImageTag {
     type: string;
     data: FileImageTagData;
 }
 
-export interface FileImageTagData{
+export interface FileImageTagData {
     src: string;
     fileId: number;
 }
 
 export class ImageMethods {
 
-    public static buildUrl(fileId: number, sizeType: string, options: CrocoHtmlOptions):string{
-        
+    public static buildUrl(fileId: number, sizeType: string, options: CrocoHtmlOptions): string {
+
+        if (fileId === null || fileId === undefined) {
+            return null;
+        }
+
         return options.publicImageResizedUrlFormat
             .replace("{sizeType}", sizeType)
             .replace("{fileId}", fileId.toString());
     }
 
-    public static buildSmallUrl(fileId: number, options: CrocoHtmlOptions):string{
+    public static buildSmallUrl(fileId: number, options: CrocoHtmlOptions): string {
         return ImageMethods.buildUrl(fileId, "Small", options);
     }
 
-    public static buildMediumUrl(fileId: number, options: CrocoHtmlOptions):string{
+    public static buildMediumUrl(fileId: number, options: CrocoHtmlOptions): string {
         return ImageMethods.buildUrl(fileId, "Medium", options);
     }
 
