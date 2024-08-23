6d0507a678d9e5cc3c2abfc279dcdf04263e387d
kirill.kokhno <kirull.kokhno@gmail.com>
2024-08-24 00:16:07 +0300

    minor fix

--- projects/croco-html/src/lib/components/editor/external-video-block/external-video-block.component.ts
+++ projects/croco-html/src/lib/components/editor/external-video-block/external-video-block.component.ts
@@ -18,17 +18,16 @@ export class ExternalVideoBlockComponent implements OnInit {
   @Output()
   onTagUpdated = new EventEmitter<HtmlBodyTag>();
 
-  defaultYoutubeLink = "https://youtu.be/jzBneaWSswY";
-  defaultVkLink = "https://vk.com/video_ext.php?oid=-22822305&id=456241864&hd=2";
+
   protected readonly ExternalVideoSupportedTypes = ExternalVideoSupportedTypes;
 
   tagData: ExternalVideoTagData = {
-    link: this.defaultYoutubeLink,
+    link: '',
     type: ExternalVideoSupportedTypes.Youtube
   };
 
   ngOnInit(): void {
-    this.tagData.link = (this.tag.attributes as ExternalVideoTagData).type === ExternalVideoSupportedTypes.VkVideo ? this.defaultVkLink : this.defaultYoutubeLink;
+    this.tagData.link = (this.tag.attributes as ExternalVideoTagData).link;
     this.tagData.type = (this.tag.attributes as ExternalVideoTagData).type
   }
 
