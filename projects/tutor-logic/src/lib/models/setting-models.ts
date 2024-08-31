9baafe2ae33fb0f5c4052790036ec2cd5343d15e
dimaserd84@gmail.com <dimaserd84@gmail.com>
2024-08-31 13:34:30 +0300

    Fixes

--- projects/tutor-logic/src/lib/models/setting-models.ts
+++ projects/tutor-logic/src/lib/models/setting-models.ts
@@ -5,46 +5,51 @@ export interface SettingModel {
 }
 
 export interface TutorFullApplicationPublicOptions {
-  applicationName: string; 
-  applicationUrl: string; 
-  telegramBotSettings: MainTelegramBotSettings; 
-  publicImageUrlFormat: string; 
-  imagesPack: TutorImagesPackSettings; 
-  icon: IconApplicatonSettings; 
-  webAppRoutes: WebAppRoutes; 
-  telephoneInputOptions: TelephoneInputOptions; 
+  applicationName: string;
+  applicationUrl: string;
+  telegramBotSettings: MainTelegramBotSettings;
+  publicImageUrlFormat: string;
+  imagesPack: TutorImagesPackSettings;
+  icon: IconApplicatonSettings;
+  webAppRoutes: WebAppRoutes;
+  telephoneInputOptions: TelephoneInputOptions;
+  solutionChatSettings: TestSolutionChatSettings;
+}
+
+export interface TestSolutionChatSettings {
+  useNewChats: boolean;
 }
 
 
 export interface MainTelegramBotSettings {
-  hasBot: boolean; 
-  botName: string; 
-  toBotWebButtonText: string; 
+  hasBot: boolean;
+  botName: string;
+  toBotWebButtonText: string;
 }
 
 
 export interface TutorImagesPackSettings {
-  error: string; 
-  success: string; 
-  solutionFinished: string; 
-  solutionCard: string; 
+  error: string;
+  success: string;
+  solutionFinished: string;
+  solutionCard: string;
 }
 
 
 export interface IconApplicatonSettings {
-  currentIconSetId: string; 
+  currentIconSetId: string;
 }
 
 
 export interface WebAppRoutes {
-  toGroupFormat: string; 
-  loginLinkFormat: string; 
-  schedules: string; 
-  materials: string; 
-  courseShop: string; 
+  toGroupFormat: string;
+  loginLinkFormat: string;
+  schedules: string;
+  materials: string;
+  courseShop: string;
 }
 
 
 export interface TelephoneInputOptions {
-  useInput: boolean; 
+  useInput: boolean;
 }
