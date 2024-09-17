6cf0a890a9636637cbfc264b89637027536a3d74
dimaserd84@gmail.com <dimaserd84@gmail.com>
2024-09-17 20:45:02 +0300

    Fixes

--- projects/croco-html/src/lib/extensions/ImageMethods.ts
+++ projects/croco-html/src/lib/extensions/ImageMethods.ts
@@ -1,23 +1,23 @@
 import { CrocoHtmlOptions } from "./HtmlExtractionMethods";
 
 export class FileImageTagDataConsts {
-    static TagName = "file-image";
-    static FileIdAttrName = "file-id";
-    static ScreenMediaRequest = "screen-media-request";
-    static DefaultValueForFileImage = "screen-width:1200,max-image-height:300;screen-width:900,max-image-height:200";
-    static ScreenWidth = 'screen-width';
-    static MaxImageHeight = 'max-image-height';
+  static TagName = "file-image";
+  static FileIdAttrName = "file-id";
+  static ScreenMediaRequest = "screen-media-request";
+  static DefaultValueForFileImage = "screen-width:1200,max-image-height:300;screen-width:900,max-image-height:200";
+  static ScreenWidth = 'screen-width';
+  static MaxImageHeight = 'max-image-height';
 }
 
 export interface FileImageTag {
-    type: string;
-    data: FileImageTagData;
+  type: string;
+  data: FileImageTagData;
 }
 
 export interface FileImageTagData {
-    src: string;
-    fileId: number;
-    screenMediaRequest: string;
+  src: string;
+  fileId: number;
+  screenMediaRequest: string;
 }
 
 export interface IMediaRequest {
@@ -27,51 +27,51 @@ export interface IMediaRequest {
 
 export class ImageMethods {
 
-    public static buildUrl(fileId: number, sizeType: string, options: CrocoHtmlOptions): string {
+  public static buildUrl(fileId: number, sizeType: string, options: CrocoHtmlOptions): string {
 
-        if (fileId === null || fileId === undefined) {
-            return null;
-        }
-
-        return options.publicImageResizedUrlFormat
-            .replace("{sizeType}", sizeType)
-            .replace("{fileId}", fileId.toString());
+    if (fileId === null || fileId === undefined) {
+      return null;
     }
 
-    public static buildSmallUrl(fileId: number, options: CrocoHtmlOptions): string {
-        return ImageMethods.buildUrl(fileId, "Small", options);
-    }
+    return options.publicImageResizedUrlFormat
+      .replace("{sizeType}", sizeType)
+      .replace("{fileId}", fileId.toString());
+  }
 
-    public static buildMediumUrl(fileId: number, options: CrocoHtmlOptions): string {
-        return ImageMethods.buildUrl(fileId, "Medium", options);
-    }
+  public static buildSmallUrl(fileId: number, options: CrocoHtmlOptions): string {
+    return ImageMethods.buildUrl(fileId, "Small", options);
+  }
 
-    public static ExtractImage(elem: HTMLElement, options: CrocoHtmlOptions): FileImageTag {
-        let fileId = +elem.getAttribute(FileImageTagDataConsts.FileIdAttrName);
-        let src = ImageMethods.buildMediumUrl(fileId, options);
-        return {
-            type: FileImageTagDataConsts.TagName,
-            data: {
-                src,
-                fileId: fileId,
-                screenMediaRequest: elem.getAttribute(FileImageTagDataConsts.ScreenMediaRequest)
-            }
-        };
-    }
+  public static buildMediumUrl(fileId: number, options: CrocoHtmlOptions): string {
+    return ImageMethods.buildUrl(fileId, "Medium", options);
+  }
+
+  public static ExtractImage(elem: HTMLElement, options: CrocoHtmlOptions): FileImageTag {
+    let fileId = +elem.getAttribute(FileImageTagDataConsts.FileIdAttrName);
+    let src = ImageMethods.buildMediumUrl(fileId, options);
+    return {
+      type: FileImageTagDataConsts.TagName,
+      data: {
+        src,
+        fileId: fileId,
+        screenMediaRequest: elem.getAttribute(FileImageTagDataConsts.ScreenMediaRequest)
+      }
+    };
+  }
 
   public static mediaRequestStringToArrayParser = (data: string) => {
-    if (!data){
+    if (!data) {
       return [];
     }
-    
-    if(!data.length) {
+
+    if (!data.length) {
       return []
     }
 
-    return  data.split(';').reduce((requests: IMediaRequest[], currentValue: string ) => {
+    return data.split(';').reduce((requests: IMediaRequest[], currentValue: string) => {
       requests.push({
-        screenWidth: ImageMethods.createMediaRequestValue(currentValue.split(','), FileImageTagDataConsts.ScreenWidth ),
-        maxImageHeight: ImageMethods.createMediaRequestValue(currentValue.split(','), FileImageTagDataConsts.MaxImageHeight ),
+        screenWidth: ImageMethods.createMediaRequestValue(currentValue.split(','), FileImageTagDataConsts.ScreenWidth),
+        maxImageHeight: ImageMethods.createMediaRequestValue(currentValue.split(','), FileImageTagDataConsts.MaxImageHeight),
       })
       return requests
     }, [])
@@ -82,22 +82,22 @@ export class ImageMethods {
   }
 
   public static mediaRequestsArrayToStringParser = (data: IMediaRequest[]) => {
-    if(!data.length) {
+    if (!data.length) {
       return ''
     }
 
-    return data.map(el=> `${FileImageTagDataConsts.ScreenWidth}:${el.screenWidth},${FileImageTagDataConsts.MaxImageHeight}:${el.maxImageHeight}`).join(';')
+    return data.map(el => `${FileImageTagDataConsts.ScreenWidth}:${el.screenWidth},${FileImageTagDataConsts.MaxImageHeight}:${el.maxImageHeight}`).join(';')
   }
 
   public static screenSizeChanged = (screenSize: number, requests: IMediaRequest[]) => {
     let newSize = null;
     requests
-      .sort((a,b) => a.screenWidth > b.screenWidth ? -1 : 1)
+      .sort((a, b) => a.screenWidth > b.screenWidth ? -1 : 1)
       .forEach((el, index) => {
-      if(screenSize < +el.screenWidth) {
-        newSize = el.maxImageHeight;
-      }
-    })
+        if (screenSize < +el.screenWidth) {
+          newSize = el.maxImageHeight;
+        }
+      })
     return newSize
   }
 
