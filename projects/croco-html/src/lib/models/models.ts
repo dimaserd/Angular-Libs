84ab6beef11287103b1562258772ae78e9ffd5da
dimaserd84@gmail.com <dimaserd84@gmail.com>
2024-09-17 20:55:14 +0300

    Fixes

--- projects/croco-html/src/lib/models/models.ts
+++ projects/croco-html/src/lib/models/models.ts
@@ -1,14 +1,15 @@
 export interface TagItem {
-    tag: string;
-    displayValue: string;
+  tag: string;
+  displayValue: string;
 }
 
 export interface HtmlBodyTag {
-    tagDescription: TagItem;
-    innerHtml: string;
-    attributes: Object;
-    presentOrEdit: boolean;
+  tagDescription: TagItem;
+  innerHtml: string;
+  attributes: Object;
+  presentOrEdit: boolean;
 }
+
 export interface AngularMaterialTableData {
   displayedColumns: string[];
   data: object[];
