699173f3d3cca0e8921fafcff9886d4f6b495c49
dimaserd84@gmail.com <dimaserd84@gmail.com>
2024-09-20 20:19:22 +0300

    Fixes

--- projects/croco-html/src/public-api.ts
+++ projects/croco-html/src/public-api.ts
@@ -6,5 +6,6 @@ export * from './lib/components';
 export * from './lib/pipes';
 export * from './lib/extensions';
 export * from './lib/croco-html.module';
-export * from './lib/models/models'
-export * from './lib/consts'
+export * from './lib/models/models';
+export * from './lib/consts';
+export * from './lib/services';
