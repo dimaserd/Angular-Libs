981dabd14993f83851846e8646fc6414a4b205d6
dimaserd84@gmail.com <dimaserd84@gmail.com>
2024-09-30 16:52:59 +0300

    Fixes

--- projects/tutor-logic/src/lib/models/course-models.ts
+++ projects/tutor-logic/src/lib/models/course-models.ts
@@ -1,48 +1,53 @@
 export interface CourseDetailedModel {
-    id: string; 
-    name: string; 
-    templateId: string; 
-    weight: number; 
-    versionId: string; 
-    themes: Array<CourseThemeDetailedModel>; 
+    id: string;
+    name: string;
+    templateId: string;
+    weight: number;
+    versionId: string;
+    themes: Array<CourseThemeDetailedModel>;
 }
 
 export interface CourseSimpleModel {
-    id: string; 
-    name: string; 
-    weight: number; 
-    versionId: string; 
+    id: string;
+    name: string;
+    weight: number;
+    versionId: string;
 }
 
 export interface CourseThemeDetailedModel {
-    id: string; 
-    name: string; 
-    description: string; 
-    weight: number; 
-    courseVersionId: string; 
-    items: Array<CourseThemeItemModel>; 
+    id: string;
+    name: string;
+    description: string;
+    weight: number;
+    courseVersionId: string;
+    items: Array<CourseThemeItemModel>;
 }
 
-
 export interface CourseThemeItemModel {
-    id: string; 
-    name: string; 
-    html: string; 
-    type: CourseThemeItemType; 
-    weight: number; 
-    templateItemId: string; 
-    test: CourseThemeItemTestModel; 
+    id: string;
+    name: string;
+    html: string;
+    type: CourseThemeItemType;
+    weight: number;
+    templateItemId: string;
+    test: CourseThemeItemTestModel;
+    uISettings: CourseThemeItemUISettingsModel;
+}
+
+export interface CourseThemeItemUISettingsModel {
+    previousSlideBtnText: string;
+    nextSlideBtnText: string;
 }
 
 
 export enum CourseThemeItemType {
-   JustHtml = 'JustHtml',
-   Test = 'Test'
+    JustHtml = 'JustHtml',
+    Test = 'Test'
 }
 
 export interface CourseThemeItemTestModel {
-    testId: string; 
-    testName: string; 
-    totalPointsToPass: number; 
-    totalPercentageToPass: number; 
+    testId: string;
+    testName: string;
+    totalPointsToPass: number;
+    totalPercentageToPass: number;
 }
