b43584ae0bddfb148bc67b08939ac5c34a6dd743
dimaserd84@gmail.com <dimaserd84@gmail.com>
2025-02-23 15:50:46 +0300

    Fixes

--- projects/croco-html/src/lib/options/index.ts
+++ projects/croco-html/src/lib/options/index.ts
@@ -1,8 +1,20 @@
-import {IMediaRequest} from "../extensions";
-import {Type} from "@angular/core";
+import { IMediaRequest } from "../extensions";
+import { Type } from "@angular/core";
 
 export interface CrocoHtmlOptions {
+
+  /**
+   * Формат сслыки для показа изображений из публичных файлов.
+   * Содержит переменные: {sizeType} - тип размера, {fileId} - идентификатор изображения.
+   */
   publicImageResizedUrlFormat: string;
+
+  /**
+   * Формат сслыки для показа изображений из приватный файлов.
+   * Содержит переменные: {sizeType} - тип размера, {fileId} - идентификатор изображения.
+   */
+  privateImageResizedUrlFormat: string;
+
   globalMediaRequests?: IMediaRequest[];
 
   /**
