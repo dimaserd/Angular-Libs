dd4095e691cc409636f780376169ad6be8e1a7f7
dimaserd84@gmail.com <dimaserd84@gmail.com>
2025-03-02 01:20:14 +0300

    Fixes

--- projects/tutor-logic/src/lib/models/student-models.ts
+++ projects/tutor-logic/src/lib/models/student-models.ts
@@ -1,41 +1,44 @@
 export interface StudentSimpleModel {
-    id: string; 
-    email: string; 
-    name: string; 
-    surname: string; 
-    patronymic: string; 
-    phoneNumber: string; 
-    isDeleting: boolean; 
-    telegramUserId: number | null; 
-    telegramUserName: string; 
+    id: string;
+    surname: string;
+    name: string;
+    patronymic: string;
+    email: string;
+    noEmail: boolean;
+    phoneNumber: string;
+    noPhoneNumber: boolean;
+    telegramUserName: string;
+    telegramUserId: number | null;
+    avatarFileId: number | null;
+    isDeleting: boolean;
 }
 
 export interface SearchStudentsRequest {
-    q: string; 
-    isDemo: boolean | null; 
-    schoolId: string; 
-    count: number | null; 
-    offSet: number; 
+    q: string;
+    isDemo: boolean | null;
+    schoolId: string;
+    count: number | null;
+    offSet: number;
 }
 
 export interface RegisterStudentRequest {
-    registrationRequest: RegisterModel; 
-    schoolId: string; 
+    registrationRequest: RegisterModel;
+    schoolId: string;
 }
 
 export interface StudentRegistrationResult {
     succeeded: boolean;
     message: string;
     studentId: string;
-  }
+}
 
 export interface RegisterModel {
-    email: string; 
-    noEmail: boolean; 
-    name: string; 
-    surname: string; 
-    patronymic: string; 
-    phoneNumber: string; 
-    noPhoneNumber: boolean; 
-    password: string; 
+    email: string;
+    noEmail: boolean;
+    name: string;
+    surname: string;
+    patronymic: string;
+    phoneNumber: string;
+    noPhoneNumber: boolean;
+    password: string;
 }
