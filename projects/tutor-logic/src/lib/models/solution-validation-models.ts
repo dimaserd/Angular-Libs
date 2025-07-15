ce15f4772048d443e216862db020e2f3a0853398
Dmitry Serdyukov <dimaserd84@gmail.com>
2025-07-15 21:17:25 +0300

    Fixes

--- projects/tutor-logic/src/lib/models/solution-validation-models.ts
+++ projects/tutor-logic/src/lib/models/solution-validation-models.ts
@@ -1,41 +1,42 @@
 import { QuestionAnswerWithResult } from "./test-solution-models";
 
 export interface TestSolutionWithPointsValidation {
-    solutionId: string; 
-    hasPointsResult: boolean; 
-    questions: Array<QuestionAnswerWithResult>; 
-    studentTotalPoints: number; 
-    maxTotalPoints: number; 
-    questionsCount: number; 
-    validatedQuestionsCount: number; 
-    inProcessQuestionsCount: number; 
-    hasRightAnswerQuestionsCount: number; 
+    solutionId: string;
+    isFinished: boolean;
+    hasPointsResult: boolean;
+    questions: Array<QuestionAnswerWithResult>;
+    studentTotalPoints: number;
+    maxTotalPoints: number;
+    questionsCount: number;
+    validatedQuestionsCount: number;
+    inProcessQuestionsCount: number;
+    hasRightAnswerQuestionsCount: number;
 }
 
 export enum QuestionValidationSourceType {
-	System = 'System',
-	Student = 'Student',
-	Curator = 'Curator'
+    System = 'System',
+    Student = 'Student',
+    Curator = 'Curator'
 }
 
 export interface TestQuestionWithModifiers {
-    questionId: string; 
-    modifiers: Array<TestQuestionModiferModel>; 
+    questionId: string;
+    modifiers: Array<TestQuestionModiferModel>;
 }
 
 export interface TestQuestionModiferModel {
-    name: string; 
-    data: string; 
+    name: string;
+    data: string;
 }
 
 export interface SaveQuestionPointsValidation {
-    idModel: QuestionInSolutionIdModel; 
-    answerPoints: number; 
-    questionTotalPoints: number; 
-    description: string; 
+    idModel: QuestionInSolutionIdModel;
+    answerPoints: number;
+    questionTotalPoints: number;
+    description: string;
 }
 
 export interface QuestionInSolutionIdModel {
-    solutionId: string; 
-    questionId: string; 
+    solutionId: string;
+    questionId: string;
 }
