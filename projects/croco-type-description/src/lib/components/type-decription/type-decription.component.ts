81f2dae9d3aecb66accd1def6907291d4cacfb95
Dmitry Serdyukov <dimaserd84@gmail.com>
2025-07-16 20:15:16 +0300

    Fixes

--- projects/croco-type-description/src/lib/components/type-decription/type-decription.component.ts
+++ projects/croco-type-description/src/lib/components/type-decription/type-decription.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CrocoTypeDescriptionResult, CrocoTypeDescription } from '../../models';
 import { MatSnackBar } from '@angular/material/snack-bar';
