04458868162a860f4bac80199d95afdb42e62e0b
Dmitry Serdyukov <dimaserd84@gmail.com>
2025-08-17 17:48:27 +0300

    Fixes

---
+++ projects/croco-html/src/lib/components/editor/main-editor-block/components/defined-custom-editor-block/defined-custom-editor-block.component.ts
@@ -0,0 +1,17 @@
+import { JsonPipe } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { HtmlBodyTag } from 'projects/croco-html/src/lib/models';
+
+@Component({
+  selector: 'croco-html-defined-custom-editor-block',
+  imports: [JsonPipe],
+  templateUrl: './defined-custom-editor-block.component.html',
+  styleUrl: './defined-custom-editor-block.component.css'
+})
+export class DefinedCustomEditorBlockComponent {
+  @Input({ required: true })
+  tag: HtmlBodyTag;
+
+  @Output()
+  onTagUpdated = new EventEmitter<HtmlBodyTag>();
+}
--- projects/croco-html/src/lib/components/editor/main-editor-block/main-editor-block.component.ts
+++ projects/croco-html/src/lib/components/editor/main-editor-block/main-editor-block.component.ts
@@ -1,7 +1,6 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Inject, Input, Output } from '@angular/core';
 import { TextTags } from '../../../extensions/TextMethods';
 import { HtmlBodyTag } from '../../../models/models';
-import { MatIcon } from '@angular/material/icon';
 import { MatIconButton } from '@angular/material/button';
 import { HtmlRawEditorComponent } from '../html-raw-editor/html-raw-editor.component';
 import { DownloadFileButtonEditorComponent } from '../download-file-button-editor/download-file-button-editor.component';
@@ -10,6 +9,8 @@ import { TextEditorComponent } from '../text-editor/text-editor.component';
 import { ExternalVideoEditorComponent } from '../external-video-editor/external-video-editor.component';
 import { ButtonEditorComponent } from "../button-editor/button-editor.component";
 import { CustomWidgetEditorComponent } from "../custom-widget-editor/custom-widget-editor.component";
+import { CrocoHtmlOptionsToken } from '../../../consts';
+import { CrocoHtmlOptions } from '../../../options';
 
 @Component({
   selector: 'croco-html-main-editor-block',
@@ -31,7 +32,7 @@ export class MainEditorBlockComponent {
 
   textTags = TextTags.allTextTags;
 
-  @Input()
+  @Input({ required: true })
   tag: HtmlBodyTag;
 
   @Output()
@@ -40,12 +41,24 @@ export class MainEditorBlockComponent {
   @Output()
   onTagRemoved = new EventEmitter<HtmlBodyTag>();
 
+  constructor(@Inject(CrocoHtmlOptionsToken) private readonly _options: CrocoHtmlOptions) {
+
+  }
+
   tagUpdatedHandler(tag: HtmlBodyTag): void {
   }
 
   save() {
     this.tag.presentOrEdit = true;
     this.onTagSaved.emit(this.tag);
+  }
+
+  isDefinedCustomTag() {
+    if (this._options.definedCustomTags.includes(this.tag.tagDescription.tag)) {
+      return true;
+    }
+
+    return false;
   }
 
   deleteItem() {
