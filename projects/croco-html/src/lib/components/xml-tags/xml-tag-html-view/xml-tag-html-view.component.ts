849d1f56e15865d5c8455a240a76b6889cf7b9e3
Dmitry Serdyukov <dimaserd84@gmail.com>
2025-11-10 23:43:27 +0300

    Fixes

--- projects/croco-html/src/lib/components/xml-tags/xml-tag-html-view/xml-tag-html-view.component.ts
+++ projects/croco-html/src/lib/components/xml-tags/xml-tag-html-view/xml-tag-html-view.component.ts
@@ -3,12 +3,9 @@ import { Component, Input } from '@angular/core';
 @Component({
     selector: 'croco-html-xml-tag-html-view',
     templateUrl: './xml-tag-html-view.component.html',
-    styleUrl: './xml-tag-html-view.component.scss',
     standalone: true
 })
 export class XmlTagHtmlViewComponent {
-
-  constructor() { }
 
   @Input()
   html: string = "";
